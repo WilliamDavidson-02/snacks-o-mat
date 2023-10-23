123051490e3711639510648c2fe48ec45c428fcd
William Davidson <wj.davidson@outlook.com>
2023-10-23 16:13:39 +0200

    cart, wallet and add snack to cart

--- index.php
+++ index.php
@@ -9,11 +9,44 @@ if (!isset($_SESSION['inventory'])) {
     $_SESSION['inventory'] = generatePriceAndInventory($inventory);
 }
 
+if (!isset($_SESSION['wallet'])) {
+    $_SESSION['wallet'] = 9999;
+}
+
+if (!isset($_SESSION['cart'])) {
+    $_SESSION['cart'] = [];
+}
+
+if (isset($_GET['snack'])) {
+    $snack = $_SESSION['inventory'][$_GET['snack']];
+    if (($_SESSION['wallet'] - $snack['price']) >= 0 && $snack['stock'] > 0) {
+        $_SESSION['cart'][] = $_GET['snack'];
+        $_SESSION['inventory'][$_GET['snack']]['stock'] -= 1;
+    }
+    // preventing same action to run again if page is reloaded.
+    header('Location: ' . $_SERVER['PHP_SELF']);
+    exit;
+}
+
 $inventory = $_SESSION['inventory'];
+$wallet = $_SESSION['wallet'];
+$cart = $_SESSION['cart'];
 
 require_once __DIR__ . '/header.php'; ?>
 
 <main>
+    <nav>
+        <div class="stock-wallet-container">
+            <form><button class="item-card-btn stock-btn" type="submit">Order new stock</button></form>
+            <div class="wallet">$<?= $wallet; ?></div>
+        </div>
+        <div class="cart">
+            <?php if (count($cart) > 0) : ?>
+                <div class="cart-notification"><?= count($cart) < 10 ? count($cart) : count($cart) . '+'; ?></div>
+            <?php endif; ?>
+            <i class="fa-solid fa-cart-shopping"></i>
+        </div>
+    </nav>
     <form method="get" class="item-card-container">
         <?php foreach ($inventory as $index => $item) : ?>
             <div class="item-card">
@@ -26,7 +59,7 @@ require_once __DIR__ . '/header.php'; ?>
                         <p class="price">$<?= $item['price']; ?></p>
                         <p class="stock">Stock <?= $item['stock']; ?></p>
                     </div>
-                    <button type="submit" class="item-card-btn">Add to cart</button>
+                    <button name="snack" value="<?= $index; ?>" type="submit" class="item-card-btn">Add to cart</button>
                 </div>
             </div>
         <?php endforeach; ?>
